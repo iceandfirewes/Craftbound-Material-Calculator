59e9407859d957b095a6f907b7e2c9f0c20bc2b1
iceandfirewes <iceandfire29072000@gmail.com>
2023-01-05 22:20:37 +0700

    add tier to options

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -3,36 +3,59 @@ import { useState } from "react"
 export default function Calculator()
 {
     //console.log(testRecursiveCraftBlueprint("leatherworking","coarse leather boots",false))
-    const [craftOption, setcraftOption] = useState({type:"leatherworking", name:"coarse leather", amount:1})
+    const [craftOption, setcraftOption] = useState({type:"leatherworking", tier:"I", name:"coarse leather", amount:1})
+    //main function to handle user input
     function handleOptionChange(event)
     {
+        /**
+         * The reason for 2 return statement here is that if the tier is changed, the name need to be changed also
+         * if it isnt changed, it would look up a tier I item in the tier II array, which break
+         */
         setcraftOption(oldCraftOption => {
-          console.log(event.target.name)
+          //tier change
+          if(event.target.name == "tier")
+          {
             return {
-                ...oldCraftOption,
-                [event.target.name]: event.target.value
+              ...oldCraftOption,
+              [event.target.name]: event.target.value,
+              name: crafts[oldCraftOption.type][event.target.value][0].name
             }
+          }
+          //everything else
+          else
+          {
+            return {
+              ...oldCraftOption,
+              [event.target.name]: event.target.value,
+            }
+          }
         })
     }   
     return <section className="calculator">
         {/*THIS USES CRAFTS OBJECTS*/}
         <div className="calculator--option">
-            <select name="type" onChange={(event) => handleOptionChange(event)} value={craftOption.name}>
+            <select name="type" onChange={(event) => handleOptionChange(event)} value={craftOption.type}>
               {Object.getOwnPropertyNames(crafts).map(profession => <option value={profession}>{profession}</option>)}
+            </select>
+            <select name="tier" onChange={(event) => handleOptionChange(event)} value={craftOption.tier}>
+              {Object.getOwnPropertyNames(crafts[craftOption.type]).map(tier => <option value={tier}>{tier}</option>)}
             </select>
             <input name="amount" type="text" value={craftOption.amount} onChange={(event) => handleOptionChange(event)}></input>
             <select name="name" onChange={(event) => handleOptionChange(event)} value={craftOption.name}>
               {/*test later once add more professions*/}
-              {crafts[craftOption.type].map(craft => (craft.hasOwnProperty("requirements") ? <option value={craft.name}>{craft.name}</option> : undefined))}
+              {crafts[craftOption.type][craftOption.tier].map(craft => (craft.hasOwnProperty("requirements") ? <option value={craft.name}>{craft.name}</option> : undefined))}
             </select>
         </div>
         {/*createCraftBlueprint(craftOption.name, craftOption.amount)*/}
-        {createCraftBluePrint(craftOption.type,craftOption.name, craftOption.amount, false)}
+        {createCraftBluePrint(craftOption.type, craftOption.tier, craftOption.name, craftOption.amount, false)}
   </section>
 }
 {/*THIS USES searchRawMaterials, craftSearch*/}
-function createCraftBluePrint(type, name, amount, rawFlag)
+//recursive blueprint generator
+function createCraftBluePrint(type, tier, name, amount, rawFlag)
 {
+  //in the event that the name is a raw material. the tier is not needed but it is in the requirements anyway
+  //may need to add tier to raw material but i dont think that nessessary
   if(rawFlag)
   {
     let rawMaterial = searchRawMaterials(type, name)
@@ -43,8 +66,8 @@ function createCraftBluePrint(type, name, amount, rawFlag)
     </div>
   }
   else{
-    let craft = craftSearch(type, name)
-    const child = craft.requirements.map(requirement => createCraftBluePrint(requirement.type, requirement.name, amount * requirement.amount, requirement.raw))
+    let craft = craftSearch(type, tier, name)
+    const child = craft.requirements.map(requirement => createCraftBluePrint(type = requirement.type, requirement.tier, requirement.name, amount * requirement.amount, requirement.raw))
     return <div className='steps'>
       <div className="ingredients">
         <span>{name}</span>
--- src/assets/Crafts.js
+++ src/assets/Crafts.js
@@ -17,49 +17,57 @@ export function searchRawMaterials(type, name)
 }
 //add more crafts
 export const crafts = {
-    leatherworking:[
+    leatherworking:{
         //TIER 1
-        {name:"coarse leather", requirements:[
-            {type:"leatherworking", name: "raw hide", amount:1, raw: true}]},
-        {name:"coarse leather boots", requirements:[{type:"leatherworking", name:"coarse leather", amount:3, raw: false}]},
-        {name:"coarse leather belt", requirements:[{type:"leatherworking", name:"coarse leather", amount:3, raw: false}]},
-        {name:"coarse leather gloves", requirements:[{type:"leatherworking", name:"coarse leather", amount:3, raw: false}]},
-        {name:"coarse leather cap", requirements:[{type:"leatherworking", name:"coarse leather", amount:4, raw: false}]},
-        {name:"coarse leather apron", requirements:[{type:"leatherworking", name:"coarse leather", amount:5, raw: false}]},
+        I:[
+            {name:"coarse leather", requirements:[
+                {type:"leatherworking", tier: "I", name: "raw hide", amount:1, raw: true}]},
+            {name:"coarse leather boots", requirements:[{type:"leatherworking", tier: "I", name:"coarse leather", amount:3, raw: false}]},
+            {name:"coarse leather belt", requirements:[{type:"leatherworking", tier: "I", name:"coarse leather", amount:3, raw: false}]},
+            {name:"coarse leather gloves", requirements:[{type:"leatherworking", tier: "I", name:"coarse leather", amount:3, raw: false}]},
+            {name:"coarse leather cap", requirements:[{type:"leatherworking", tier: "I", name:"coarse leather", amount:4, raw: false}]},
+            {name:"coarse leather apron", requirements:[{type:"leatherworking", tier: "I", name:"coarse leather", amount:5, raw: false}]},
+        ],
         //TIER 2
-        {name:"light leather", requirements:[
-            {type:"leatherworking", name:"light hide", amount:2, raw: true},
-            {type:"leatherworking", name:"tannin", amount:1, raw: true}]},
-        {name:"light leather boots", requirements:[{type:"leatherworking", name:"light leather", amount:3, raw: false}]},
-        {name:"light leather belt", requirements:[{type:"leatherworking", name:"light leather", amount:3, raw: false}]},
-        {name:"light leather gloves", requirements:[{type:"leatherworking", name:"light leather", amount:3, raw: false}]},
-        {name:"light leather cap", requirements:[{type:"leatherworking", name:"light leather", amount:4, raw: false}]},
-        {name:"light leather apron", requirements:[{type:"leatherworking", name:"light leather", amount:5, raw: false}]},
+        II:[
+            {name:"light leather", requirements:[
+                {type:"leatherworking", tier: "II", name:"light hide", amount:2, raw: true},
+                {type:"leatherworking", tier: "II", name:"tannin", amount:1, raw: true}]},
+            {name:"light leather boots", requirements:[{type:"leatherworking", tier: "II", name:"light leather", amount:3, raw: false}]},
+            {name:"light leather belt", requirements:[{type:"leatherworking", tier: "II", name:"light leather", amount:3, raw: false}]},
+            {name:"light leather gloves", requirements:[{type:"leatherworking", tier: "II", name:"light leather", amount:3, raw: false}]},
+            {name:"light leather cap", requirements:[{type:"leatherworking", tier: "II", name:"light leather", amount:4, raw: false}]},
+            {name:"light leather apron", requirements:[{type:"leatherworking", tier: "II", name:"light leather", amount:5, raw: false}]},
+        ],
         //TIER 3
-        {name:"heavy leather", requirements:[
-            {type:"leatherworking", name:"heavy hide", amount:3, raw: true},
-            {type:"leatherworking", name:"rested tannin", amount:1, raw: true},
-            {type:"leatherworking", name:"light leather", amount:1, raw: false}]},
-        {name:"heavy leather boots", requirements:[{type:"leatherworking", name:"heavy leather", amount:3, raw: false}]},
-        {name:"heavy leather belt", requirements:[{type:"leatherworking", name:"heavy leather", amount:3, raw: false}]},
-        {name:"heavy leather gloves", requirements:[{type:"leatherworking", name:"heavy leather", amount:3, raw: false}]},
-        {name:"heavy leather cap", requirements:[{type:"leatherworking", name:"heavy leather", amount:4, raw: false}]},
-        {name:"heavy leather apron", requirements:[{type:"leatherworking", name:"heavy leather", amount:5, raw: false}]},
+        III:[
+            {name:"heavy leather", requirements:[
+                {type:"leatherworking", tier: "III", name:"heavy hide", amount:3, raw: true},
+                {type:"leatherworking", tier: "III", name:"rested tannin", amount:1, raw: true},
+                {type:"leatherworking", tier: "II", name:"light leather", amount:1, raw: false}]},
+            {name:"heavy leather boots", requirements:[{type:"leatherworking", tier: "III", name:"heavy leather", amount:3, raw: false}]},
+            {name:"heavy leather belt", requirements:[{type:"leatherworking", tier: "III", name:"heavy leather", amount:3, raw: false}]},
+            {name:"heavy leather gloves", requirements:[{type:"leatherworking", tier: "III", name:"heavy leather", amount:3, raw: false}]},
+            {name:"heavy leather cap", requirements:[{type:"leatherworking", tier: "III", name:"heavy leather", amount:4, raw: false}]},
+            {name:"heavy leather apron", requirements:[{type:"leatherworking", tier: "III", name:"heavy leather", amount:5, raw: false}]},
+        ],
         //TIER 4
-        {name:"rugged leather", requirements:[
-            {type:"leatherworking", name:"rugged hide", amount:4, raw: true},
-            {type:"leatherworking", name:"aged tannin", amount:1, raw: true},
-            {type:"leatherworking", name:"heavy leather", amount:1, raw: false}]},
-        {name:"rugged leather boots", requirements:[{type:"leatherworking", name:"rugged leather", amount:3, raw: false}]},
-        {name:"rugged leather belt", requirements:[{type:"leatherworking", name:"rugged leather", amount:3, raw: false}]},
-        {name:"rugged leather gloves", requirements:[{type:"leatherworking", name:"rugged leather", amount:3, raw: false}]},
-        {name:"rugged leather cap", requirements:[{type:"leatherworking", name:"rugged leather", amount:4, raw: false}]},
-        {name:"rugged leather apron", requirements:[{type:"leatherworking", name:"rugged leather", amount:5, raw: false}]},
-    ]
+        IV:[
+            {name:"rugged leather", requirements:[
+                {type:"leatherworking", tier: "IV", name:"rugged hide", amount:4, raw: true},
+                {type:"leatherworking", tier: "IV", name:"aged tannin", amount:1, raw: true},
+                {type:"leatherworking", tier: "III", name:"heavy leather", amount:1, raw: false}]},
+            {name:"rugged leather boots", requirements:[{type:"leatherworking", tier: "IV", name:"rugged leather", amount:3, raw: false}]},
+            {name:"rugged leather belt", requirements:[{type:"leatherworking", tier: "IV", name:"rugged leather", amount:3, raw: false}]},
+            {name:"rugged leather gloves", requirements:[{type:"leatherworking", tier: "IV", name:"rugged leather", amount:3, raw: false}]},
+            {name:"rugged leather cap", requirements:[{type:"leatherworking", tier: "IV", name:"rugged leather", amount:4, raw: false}]},
+            {name:"rugged leather apron", requirements:[{type:"leatherworking", tier: "IV", name:"rugged leather", amount:5, raw: false}]},
+        ]
+    }
 }
-export function craftSearch(type, name)
+export function craftSearch(type, tier, name)
 {
-    return crafts[type].find(craft => craft.name == name)
+    return crafts[type][tier].find(craft => craft.name == name)
 }
 
 
