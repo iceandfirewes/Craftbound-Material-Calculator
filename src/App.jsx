ca31258706471d6f9605c8f4f425c4e3d43403b4
iceandfirewes <iceandfire29072000@gmail.com>
2023-01-14 22:59:05 +0700

    small css change

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Calculator from './Calculator.jsx'
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <h1>This is a simple calculator to tell how much material is needed for a given craft</h1>
       <Calculator />
       <div className="update">1/14 - update UI</div>
